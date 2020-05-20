fd87d0ccb77f436ed827e477fdd750f3d5e9bced
Malik Adebiyi <33009736+ma3trix@users.noreply.github.com>
2020-05-20 02:37:27 -0400

    Update diceView.swift

--- diceView.swift
+++ diceView.swift
@@ -2,9 +2,7 @@
 //  diceView.swift
 //  WarCardGame
 //
-//  Created by Oluwafimihan Adebiyi on 2019-09-05.
-//  Copyright © 2019 Christopher Ching. All rights reserved.
-//
+
 
 import Foundation
 
