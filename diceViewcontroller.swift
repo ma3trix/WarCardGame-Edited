00f756c7f1d08aa01142162700d1c71eaf04f94e
Malik Adebiyi <33009736+ma3trix@users.noreply.github.com>
2020-05-20 02:37:51 -0400

    Update diceViewcontroller.swift

--- diceViewcontroller.swift
+++ diceViewcontroller.swift
@@ -2,9 +2,7 @@
 //  diceViewcontroller.swift
 //  WarCardGame
 //
-//  Created by Oluwafimihan Adebiyi on 2019-09-05.
-//  Copyright © 2019 Christopher Ching. All rights reserved.
-//
+
 
 import Foundation
 import UIKit
